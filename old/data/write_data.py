62bb1c843187e7e1aa07c15601b3765c59bb5a39
Kyle <kylesch115@gmail.com>
2025-01-22 13:23:13 -0700

    remove old/ code

--- old/data/write_data.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def write_data():
-    with open("monitors.json") as f:
-        data = json.load(f)
-    print(data)
-
-if __name__ == "__main__":
-    write_data()
--- old/src/admsim/distribution/test.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(Teest, testste)
-{
-  ASSERT_TRUE(true);
-}
